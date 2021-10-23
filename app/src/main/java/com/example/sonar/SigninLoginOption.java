6725fc145f3f2c37287df35d311fa5690b969215
Tiba <tibaalanssari53@gmail.com>
2021-10-23 14:51:15 -0500

    first commit

---
+++ app/src/main/java/com/example/sonar/SigninLoginOption.java
@@ -0,0 +1,14 @@
+package com.example.sonar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SigninLoginOption extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signin_login_option);
+    }
+}
