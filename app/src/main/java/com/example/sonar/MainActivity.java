208ff5ff3e5c8bbca456295798304370b4a2ded1
Vanessa <hpj766@my.utsa.edu>
2021-10-24 10:41:27 -0500

    Alert retrieves location

--- app/src/main/java/com/example/sonar/MainActivity.java
+++ app/src/main/java/com/example/sonar/MainActivity.java
@@ -2,9 +2,12 @@ package com.example.sonar;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.widget.Toast;
--- app/src/main/java/com/example/sonar/StartActivity.java
+++ app/src/main/java/com/example/sonar/StartActivity.java
@@ -1,8 +1,11 @@
 package com.example.sonar;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -18,6 +21,8 @@ public class StartActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+
+        verifyPermissions();
 
         // if the user is already logged in, redirect to Alert Activity
         if (ParseUser.getCurrentUser() != null) { // user is logged in
@@ -40,6 +45,16 @@ public class StartActivity extends AppCompatActivity {
                 goToLoginPage();
             }
         });
+    }
+
+    private void verifyPermissions() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION, },
+                    PackageManager.PERMISSION_GRANTED);
+            return;
+        }
     }
 
     private void goToSignUpPage() {
