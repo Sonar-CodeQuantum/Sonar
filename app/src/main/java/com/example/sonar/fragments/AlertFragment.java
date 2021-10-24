fe9944e4b6c4e0ee8ebb9d125de5c027cbfc0548
Vanessa <hpj766@my.utsa.edu>
2021-10-24 04:12:53 -0500

    ui touches

--- app/src/main/java/com/example/sonar/fragments/AlertFragment.java
+++ app/src/main/java/com/example/sonar/fragments/AlertFragment.java
@@ -1,4 +1,4 @@
-package com.example.sonar.Fragments;
+package com.example.sonar.fragments;
 
 import android.os.Bundle;
 
@@ -7,26 +7,30 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
 import com.example.sonar.R;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link ProfileFragment#newInstance} factory method to
+ * Use the {@link AlertFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class ProfileFragment extends Fragment {
+public class AlertFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
+    private ImageButton alertbutton;
+    private int alertState = 0;
+
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
 
-    public ProfileFragment() {
+    public AlertFragment() {
         // Required empty public constructor
     }
 
@@ -36,11 +40,11 @@ public class ProfileFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment ProfileFragment.
+     * @return A new instance of fragment AlertFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static ProfileFragment newInstance(String param1, String param2) {
-        ProfileFragment fragment = new ProfileFragment();
+    public static AlertFragment newInstance(String param1, String param2) {
+        AlertFragment fragment = new AlertFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -61,6 +65,28 @@ public class ProfileFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        return inflater.inflate(R.layout.fragment_alert, container, false);
+
+
+    }
+
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        // Setup any handles to view objects here
+        alertbutton = view.findViewById(R.id.alertbutton);
+        alertbutton.setBackgroundResource(R.drawable.alert_button);
+        alertbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(alertState == 0)
+                {
+                    alertbutton.setBackgroundResource(R.drawable.alert_button_red);
+                    alertState = 1;
+                }
+                else{
+                    alertbutton.setBackgroundResource(R.drawable.alert_button);
+                    alertState = 0;
+                }
+            }
+        });
     }
 }
--- app/src/main/java/com/example/sonar/fragments/FriendsListFragment.java
+++ app/src/main/java/com/example/sonar/fragments/FriendsListFragment.java
@@ -1,4 +1,4 @@
-package com.example.sonar.Fragments;
+package com.example.sonar.fragments;
 
 import android.os.Bundle;
 
@@ -7,32 +7,26 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.ImageButton;
 
-import com.bumptech.glide.Glide;
 import com.example.sonar.R;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link AlertFragment#newInstance} factory method to
+ * Use the {@link FriendsListFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class AlertFragment extends Fragment {
+public class FriendsListFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    private ImageButton alertbutton;
-    private int alertState = 0;
-
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
 
-    public AlertFragment() {
+    public FriendsListFragment() {
         // Required empty public constructor
     }
 
@@ -42,11 +36,11 @@ public class AlertFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment AlertFragment.
+     * @return A new instance of fragment FriendsListFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static AlertFragment newInstance(String param1, String param2) {
-        AlertFragment fragment = new AlertFragment();
+    public static FriendsListFragment newInstance(String param1, String param2) {
+        FriendsListFragment fragment = new FriendsListFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -67,28 +61,6 @@ public class AlertFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_alert, container, false);
-
-
-    }
-
-    public void onViewCreated(View view, Bundle savedInstanceState) {
-        // Setup any handles to view objects here
-        alertbutton = view.findViewById(R.id.alertbutton);
-        alertbutton.setBackgroundResource(R.drawable.alert_button);
-        alertbutton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(alertState == 0)
-                {
-                    alertbutton.setBackgroundResource(R.drawable.alert_button_red);
-                    alertState = 1;
-                }
-                else{
-                    alertbutton.setBackgroundResource(R.drawable.alert_button);
-                    alertState = 0;
-                }
-            }
-        });
+        return inflater.inflate(R.layout.fragment_friends_list, container, false);
     }
 }
