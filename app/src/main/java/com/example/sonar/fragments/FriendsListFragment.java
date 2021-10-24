82b33936b6a915b4d9bd6e3d8afc318357043297
Vanessa <hpj766@my.utsa.edu>
2021-10-24 05:17:12 -0500

    made add friend view

--- app/src/main/java/com/example/sonar/fragments/FriendsListFragment.java
+++ app/src/main/java/com/example/sonar/fragments/FriendsListFragment.java
@@ -1,18 +1,25 @@
 package com.example.sonar.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
+import com.example.sonar.MainActivity;
 import com.example.sonar.R;
+import com.parse.ParseUser;
 
 public class FriendsListFragment extends Fragment {
+
+    LinearLayout addFriendContainer;
 
     public FriendsListFragment() {
         // Required empty public constructor
@@ -29,6 +36,21 @@ public class FriendsListFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        addFriendContainer = view.findViewById(R.id.addFriendContainer);
 
+        addFriendContainer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                gotoAddFriendFragment();
+            }
+
+            private void gotoAddFriendFragment() {
+                AppCompatActivity activity = (AppCompatActivity) view.getContext();
+                activity.getSupportFragmentManager().beginTransaction().
+                        replace(R.id.flContainer, new AddFriendFragment()).
+                        addToBackStack(null).
+                        commit();
+            }
+        });
     }
 }
